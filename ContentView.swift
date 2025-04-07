f5f1fc9fedff47fdeb812339f0cc7d933c2463a8
Calliope Epstein-Pawlak <kiki360@rootlike.com>
2025-04-07 13:30:36 -0500

    I added a button that will increase the size of the text with my name.

--- ContentView.swift
+++ ContentView.swift
@@ -1,9 +1,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var textSize = 100.00
     var body: some View {
         VStack {
             Text("Calliope")
+                .frame(width: textSize, height: textSize/2)
+                .background(.green)
+                .clipShape(RoundedRectangle(cornerRadius: 25.0))
+            
+            Button {
+                withAnimation(.easeIn) {
+                    textSize += 10.0
+                }
+            } label: {
+                RoundedRectangle(cornerRadius: 25.0)
+                    .frame(width: 150, height: 75)
+                    .overlay {
+                        Text("Increase Size")
+                            .foregroundStyle(.black)
+                    }
+            }
+
         }
     }
 }
