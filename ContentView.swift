de6f40dd503aad890e7bd249c0d7921c7efd3608
Alexander Kuklinski <akuklinski7210@stu.d214.org>
2025-04-07 13:24:27 -0500

    i added a textfield where you enter your name

--- ContentView.swift
+++ ContentView.swift
@@ -1,12 +1,15 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var name = ""
+    @State var update = "Alex"
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        Text("\(update)")
+        TextField("Enter Name", text: $name)
+        Button{
+           update = name
+        }label: {
+            Text("update")
         }
     }
 }
