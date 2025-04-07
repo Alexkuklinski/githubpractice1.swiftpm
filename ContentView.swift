c82dcb76bd4d98ab61aeffb53aaf9bd4d2680161
Calliope Epstein-Pawlak <kiki360@rootlike.com>
2025-04-07 13:32:27 -0500

    I fixed an issue with the opening and closing brackets that was causing an error.

--- ContentView.swift
+++ ContentView.swift
@@ -8,28 +8,29 @@ struct ContentView: View {
         Text("\(update)")
         TextField("Enter Name", text: $name)
         Button{
-           update = name
+            update = name
         }label: {
             Text("update")
-        VStack {
-            Text("Calliope")
-                .frame(width: textSize, height: textSize/2)
-                .background(.green)
-                .clipShape(RoundedRectangle(cornerRadius: 25.0))
-            
-            Button {
-                withAnimation(.easeIn) {
-                    textSize += 10.0
-                }
-            } label: {
-                RoundedRectangle(cornerRadius: 25.0)
-                    .frame(width: 150, height: 75)
-                    .overlay {
-                        Text("Increase Size")
-                            .foregroundStyle(.black)
+        }
+            VStack {
+                Text("Calliope")
+                    .frame(width: textSize, height: textSize/2)
+                    .background(.green)
+                    .clipShape(RoundedRectangle(cornerRadius: 25.0))
+                
+                Button {
+                    withAnimation(.easeIn) {
+                        textSize += 10.0
                     }
+                } label: {
+                    RoundedRectangle(cornerRadius: 25.0)
+                        .frame(width: 150, height: 75)
+                        .overlay {
+                            Text("Increase Size")
+                                .foregroundStyle(.black)
+                        }
+                }
+                
             }
-
         }
     }
-}
