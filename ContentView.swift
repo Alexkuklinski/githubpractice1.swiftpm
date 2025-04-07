304d6e3cfa058276801f1ffa764797985c29a8db
Calliope Epstein-Pawlak <kiki360@rootlike.com>
2025-04-07 13:38:18 -0500

    Added a button to decrease its size and the size of another button. I also added a text with both of our names

--- ContentView.swift
+++ ContentView.swift
@@ -2,9 +2,14 @@ import SwiftUI
 
 struct ContentView: View {
     @State var textSize = 100.00
+    
     @State var name = ""
     @State var update = "Alex"
     var body: some View {
+        Text("Made by: Alex and Calliope")
+        
+        Spacer(minLength: 100)
+        
         Text("\(update)")
         TextField("Enter Name", text: $name)
         Button{
@@ -13,10 +18,17 @@ struct ContentView: View {
             Text("update")
         }
             VStack {
-                Text("Calliope")
-                    .frame(width: textSize, height: textSize/2)
-                    .background(.green)
-                    .clipShape(RoundedRectangle(cornerRadius: 25.0))
+                Button {
+                    withAnimation(.easeOut) {
+                        textSize -= 10.0
+                    }
+                } label: {
+                    Text("Decrease Size")
+                        .frame(width: textSize, height: textSize/2)
+                        .background(.green)
+                        .foregroundStyle(.black)
+                        .clipShape(RoundedRectangle(cornerRadius: 25.0))
+                }
                 
                 Button {
                     withAnimation(.easeIn) {
@@ -24,12 +36,14 @@ struct ContentView: View {
                     }
                 } label: {
                     RoundedRectangle(cornerRadius: 25.0)
-                        .frame(width: 150, height: 75)
+                        .frame(width: textSize * 2, height: textSize)
                         .overlay {
                             Text("Increase Size")
                                 .foregroundStyle(.black)
                         }
                 }
+                
+                Spacer()
                 
             }
         }
